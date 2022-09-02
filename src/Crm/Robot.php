74f668b1fb243226831002538f2f054551a3af38
10947 <zx@addcn.com>
2022-09-02 16:55:05 +0800

    122121

--- src/Crm/Robot.php
+++ src/Crm/Robot.php
@@ -7,6 +7,7 @@
  * @contact  hdj@addcn.com
  * @license  https://code.addcn.com/8591/mas/blob/master/LICENSE
  */
+
 namespace Trigold\Udesk\Crm;
 
 use RuntimeException;
@@ -24,10 +25,10 @@ class Robot
     /**
      * 自动外呼-获取任务列表.
      *
-     * @param int $pageNum 页码
-     * @param int $pageSize 每页数量，默认20，最大50
-     * @param null|string $name 任务名称
-     * @param null|int $status 任务状态(1,进行中, 2,暂停中, 3,暂停, 4,完成, 5,归档)
+     * @param  int          $pageNum   页码
+     * @param  int          $pageSize  每页数量，默认20，最大50
+     * @param  null|string  $name      任务名称
+     * @param  null|int     $status    任务状态(1,进行中, 2,暂停中, 3,暂停, 4,完成, 5,归档)
      */
     public function getTasks(int $pageNum = 1, int $pageSize = 10, string $name = null, int $status = null): array
     {
@@ -38,7 +39,7 @@ class Robot
         if ($status) {
             $parameters['status'] = $status;
         }
-        $resp    = HttpClient::get($this->app->url('/api/v1/autoCallTasks'), $parameters);
+        $resp = HttpClient::get($this->app->url('/api/v1/autoCallTasks'), $parameters);
         $decoded = json_decode($resp['body'], true);
 
         if ($decoded['code'] != 200) {
@@ -49,23 +50,24 @@ class Robot
 
     /**
      * 创建任务接口.
-     * @param string $name 任务名称
-     * @param int $priority 优先级
-     * @param null|int $userGroupId 技能组
-     * @param int $type 任务类型(1精准,2比率,3预测,4语音)
-     * @param int $startMode 启动方式(1手动2定时)
-     * @param int $scheduleId 工作时间
-     * @param int $ivrRouterId AI话术
-     * @param string $calloutNumber 主叫号码
-     * @param int $callLimit 并发数
-     * @param int $ringOutTime 振铃超时时长
-     * @param int $spNumberType 中断号类型(1中断号2号码池)
-     * @param string $startTime 启动时间(定时使用yyyy-MM-dd HH:mm:ss)
-     * @param string $redialScene 挂机原因
-     * @param int $redialTimes 重呼次数(1~6次)
-     * @param int $redialSpace 重呼间隔类型(1呼叫单个号码后定时重呼2任务号码全部执行一次后重呼)
-     * @param string $redialGuide 重呼间隔(1～60分钟)
-     * @param string $remark 描述
+     *
+     * @param  string    $name           任务名称
+     * @param  int       $priority       优先级
+     * @param  null|int  $userGroupId    技能组
+     * @param  int       $type           任务类型(1精准,2比率,3预测,4语音)
+     * @param  int       $startMode      启动方式(1手动2定时)
+     * @param  int       $scheduleId     工作时间
+     * @param  int       $ivrRouterId    AI话术
+     * @param  string    $calloutNumber  主叫号码
+     * @param  int       $callLimit      并发数
+     * @param  int       $ringOutTime    振铃超时时长
+     * @param  int       $spNumberType   中断号类型(1中断号2号码池)
+     * @param  string    $startTime      启动时间(定时使用yyyy-MM-dd HH:mm:ss)
+     * @param  string    $redialScene    挂机原因
+     * @param  int       $redialTimes    重呼次数(1~6次)
+     * @param  int       $redialSpace    重呼间隔类型(1呼叫单个号码后定时重呼2任务号码全部执行一次后重呼)
+     * @param  string    $redialGuide    重呼间隔(1～60分钟)
+     * @param  string    $remark         描述
      *
      * @return array
      */
@@ -121,7 +123,7 @@ class Robot
             $parameters['startTime'] = $startTime;
         }
 
-        $resp    = HttpClient::post($this->app->url('/api/v1/autoCallTasks'), $parameters);
+        $resp = HttpClient::post($this->app->url('/api/v1/autoCallTasks'), $parameters);
         $decoded = json_decode($resp['body'], true);
         if ($decoded['code'] != 200) {
             throw new RuntimeException($decoded['message']);
@@ -132,13 +134,13 @@ class Robot
     /**
      * 获取指定任务详情接口.
      *
-     * @param int $id 任务id
+     * @param  int  $id  任务id
      *
      * @return array
      */
     public function getCallTasksById(int $id): ?array
     {
-        $resp    = HttpClient::get($this->app->url('/api/v1/autoCallTasks/' . $id), []);
+        $resp = HttpClient::get($this->app->url('/api/v1/autoCallTasks/'.$id), []);
         $decoded = json_decode($resp['body'], true);
         if ($decoded['code'] != 200) {
             throw new RuntimeException($decoded['message']);
@@ -149,17 +151,17 @@ class Robot
     /**
      * 更新任务接口.
      *
-     * @param int $id 任务id
-     * @param string $name 任务名称
-     * @param int $priority 优先级
-     * @param int $startMode 启动方式(1手动2定时)
-     * @param int $scheduleId 工作时间
-     * @param string $calloutNumber 主叫号码
-     * @param int $callLimit 并发数
-     * @param int $ringOutTime 振铃超时时长
-     * @param int $spNumberType 中断号类型(1中断号2号码池)
-     * @param string $startTime 启动时间(定时使用yyyy-MM-dd HH:mm:ss)
-     * @param string $remark 描述
+     * @param  int     $id             任务id
+     * @param  string  $name           任务名称
+     * @param  int     $priority       优先级
+     * @param  int     $startMode      启动方式(1手动2定时)
+     * @param  int     $scheduleId     工作时间
+     * @param  string  $calloutNumber  主叫号码
+     * @param  int     $callLimit      并发数
+     * @param  int     $ringOutTime    振铃超时时长
+     * @param  int     $spNumberType   中断号类型(1中断号2号码池)
+     * @param  string  $startTime      启动时间(定时使用yyyy-MM-dd HH:mm:ss)
+     * @param  string  $remark         描述
      *
      * @return array
      */
@@ -194,7 +196,7 @@ class Robot
             $parameters['remark'] = $remark;
         }
 
-        $resp    = HttpClient::put($this->app->url('/api/v1/autoCallTasks/' . $id), $parameters);
+        $resp = HttpClient::put($this->app->url('/api/v1/autoCallTasks/'.$id), $parameters);
         $decoded = json_decode($resp['body'], true);
         if ($decoded['code'] != 200) {
             throw new RuntimeException($decoded['message']);
@@ -205,15 +207,15 @@ class Robot
     /**
      * 启动任务接口.
      *
-     * @param int $id 任务id
+     * @param  int  $id  任务id
      *
      * @return array
      */
     public function startCallTasks(int $id): ?array
     {
         $parameters = compact('id');
-        $resp       = HttpClient::post($this->app->url('/api/v1/autoCallTasks/status/start/') . '&id=' . $id, $parameters);
-        $decoded    = json_decode($resp['body'], true);
+        $resp = HttpClient::post($this->app->url('/api/v1/autoCallTasks/status/start/').'&id='.$id, $parameters);
+        $decoded = json_decode($resp['body'], true);
         if ($decoded['code'] != 200) {
             throw new RuntimeException($decoded['message']);
         }
@@ -223,13 +225,13 @@ class Robot
     /**
      * 暂停任务接口.
      *
-     * @param int $id 任务id
+     * @param  int  $id  任务id
      *
      * @return array
      */
     public function pauseCallTasks(int $id): ?array
     {
-        $resp    = HttpClient::post($this->app->url('/api/v1/autoCallTasks/status/pause') . "&id={$id}", []);
+        $resp = HttpClient::post($this->app->url('/api/v1/autoCallTasks/status/pause')."&id={$id}", []);
         $decoded = json_decode($resp['body'], true);
         if ($decoded['code'] != 200) {
             throw new RuntimeException($decoded['message']);
@@ -240,13 +242,13 @@ class Robot
     /**
      * 归档任务接口.
      *
-     * @param int $id 任务id
+     * @param  int  $id  任务id
      *
      * @return array
      */
     public function archiveCallTasks(int $id): ?array
     {
-        $resp    = HttpClient::post($this->app->url('/api/v1/autoCallTasks/status/archive') . "&id={$id}");
+        $resp = HttpClient::post($this->app->url('/api/v1/autoCallTasks/status/archive')."&id={$id}");
         $decoded = json_decode($resp['body'], true);
         if ($decoded['code'] != 200) {
             throw new RuntimeException($decoded['message']);
@@ -255,26 +257,26 @@ class Robot
     }
 
     /**
-     * @param int $outboundTaskId 任务ID
-     * @param int $callResult 最近外呼结果(1已接通, 2未接通, 7未拨打)
-     * @param int $durationMin 通话时长左值
-     * @param int $durationMax 通话时长右值
-     * @param int $talkTurn 对话轮次
-     * @param int $pageNum 页码
-     * @param int $pageSize 每页数量，默认20，最大50
-     * @param string $redialTimeStartStr 查询拨号开始时间(例：2019-01-01 13:01:01)
-     * @param string $redialTimeEndStr 查询拨号结止时间(例：2019-01-01 15:01:01)
+     * @param  int       $outboundTaskId      任务ID
+     * @param  int       $pageNum             页码
+     * @param  int       $pageSize            每页数量，默认20，最大50
+     * @param  int|null  $callResult          最近外呼结果(1已接通, 2未接通, 7未拨打)
+     * @param  int|null  $durationMin         通话时长左值
+     * @param  int|null  $durationMax         通话时长右值
+     * @param  int|null  $talkTurn            对话轮次
+     * @param  string    $redialTimeStartStr  查询拨号开始时间(例：2019-01-01 13:01:01)
+     * @param  string    $redialTimeEndStr    查询拨号结止时间(例：2019-01-01 15:01:01)
      *
      * @return array
      */
     public function getCallTasksNumber(
         int $outboundTaskId,
-        int $callResult,
-        int $durationMin,
-        int $durationMax,
-        int $talkTurn,
         int $pageNum = 1,
         int $pageSize = 20,
+        int $callResult = null,
+        int $durationMin = null,
+        int $durationMax = null,
+        int $talkTurn = null,
         string $redialTimeStartStr = '',
         string $redialTimeEndStr = ''
     ): ?array {
@@ -295,7 +297,7 @@ class Robot
             $parameters['redialTimeEndStr'] = $redialTimeEndStr;
         }
 
-        $resp    = HttpClient::get($this->app->url('/api/v1/autoCallTasks/number'), $parameters);
+        $resp = HttpClient::get($this->app->url('/api/v1/autoCallTasks/number'), $parameters);
         $decoded = json_decode($resp['body'], true);
         if ($decoded['code'] != 200) {
             throw new RuntimeException($decoded['message']);
@@ -304,15 +306,15 @@ class Robot
     }
 
     /**
-     * @param int $numberId 号码ID
+     * @param  int  $numberId  号码ID
      *
      * @return array
      */
     public function getCallTasksNumberByNumberId(int $numberId): ?array
     {
         $parameters = compact('numberId');
-        $resp       = HttpClient::get($this->app->url('/api/v1/autoCallTasks/number/' . $numberId));
-        $decoded    = json_decode($resp['body'], true);
+        $resp = HttpClient::get($this->app->url('/api/v1/autoCallTasks/number/'.$numberId));
+        $decoded = json_decode($resp['body'], true);
         if ($decoded['code'] != 200) {
             throw new RuntimeException($decoded['message']);
         }
@@ -322,19 +324,19 @@ class Robot
     /**
      * 向任务中导入客户接口(同步).
      *
-     * @param int $outboundTaskId 任务ID
-     * @param int $dealType 重复处理类型(1全局2所有任务3当前任务4不查重)
-     * @param array[] $numberList 号码数组
-     *                            name       姓名
-     *                            mobile     号码数组
-     *                            remark     备注
-     *                            variates   自定义变量(格式:[{"key":"weather","type":"string","value":"北京"}])
+     * @param  int      $outboundTaskId  任务ID
+     * @param  int      $dealType        重复处理类型(1全局2所有任务3当前任务4不查重)
+     * @param  array[]  $numberList      号码数组
+     *                                   name       姓名
+     *                                   mobile     号码数组
+     *                                   remark     备注
+     *                                   variates   自定义变量(格式:[{"key":"weather","type":"string","value":"北京"}])
      */
     public function callTasksSyncNumber(int $outboundTaskId, int $dealType, array $numberList): ?array
     {
         $parameters = compact('outboundTaskId', 'dealType', 'numberList');
-        $resp       = HttpClient::post($this->app->url('/api/v1/autoCallTasks/syncNumber'), $parameters);
-        $decoded    = json_decode($resp['body'], true);
+        $resp = HttpClient::post($this->app->url('/api/v1/autoCallTasks/syncNumber'), $parameters);
+        $decoded = json_decode($resp['body'], true);
         if ($decoded['code'] != 200) {
             throw new RuntimeException($decoded['message']);
         }
@@ -344,19 +346,19 @@ class Robot
     /**
      * 向任务中导入客户接口(异步).
      *
-     * @param int $outboundTaskId 任务ID
-     * @param int $dealType 重复处理类型(1全局2所有任务3当前任务4不查重)
-     * @param array[] $numberList 号码数组
-     *                            name       姓名
-     *                            mobile     号码数组
-     *                            remark     备注
-     *                            variates   自定义变量(格式:[{"key":"weather","type":"string","value":"北京"}])
+     * @param  int      $outboundTaskId  任务ID
+     * @param  int      $dealType        重复处理类型(1全局2所有任务3当前任务4不查重)
+     * @param  array[]  $numberList      号码数组
+     *                                   name       姓名
+     *                                   mobile     号码数组
+     *                                   remark     备注
+     *                                   variates   自定义变量(格式:[{"key":"weather","type":"string","value":"北京"}])
      */
     public function callTasksAsyncNumber(int $outboundTaskId, int $dealType, array $numberList): ?array
     {
         $parameters = compact('outboundTaskId', 'dealType', 'numberList');
-        $resp       = HttpClient::post($this->app->url('/api/v1/autoCallTasks/asyncNumber'), $parameters);
-        $decoded    = json_decode($resp['body'], true);
+        $resp = HttpClient::post($this->app->url('/api/v1/autoCallTasks/asyncNumber'), $parameters);
+        $decoded = json_decode($resp['body'], true);
         if ($decoded['code'] != 200) {
             throw new RuntimeException($decoded['message']);
         }
@@ -371,8 +373,8 @@ class Robot
     public function getCallTasksSyncNumberResult(int $outboundTaskId, string $batchId): array
     {
         $parameters = compact('outboundTaskId', 'batchId');
-        $resp       = HttpClient::post($this->app->url('/api/v1/autoCallTasks/syncNumberResult'), $parameters);
-        $decoded    = json_decode($resp['body'], true);
+        $resp = HttpClient::post($this->app->url('/api/v1/autoCallTasks/syncNumberResult'), $parameters);
+        $decoded = json_decode($resp['body'], true);
         if ($decoded['code'] != 200) {
             throw new RuntimeException($decoded['message']);
         }
@@ -388,7 +390,7 @@ class Robot
         if ($id) {
             $parameters['id'] = $id;
         }
-        $resp    = HttpClient::post($this->app->url('/api/v1/targetUrls'), $parameters);
+        $resp = HttpClient::post($this->app->url('/api/v1/targetUrls'), $parameters);
         $decoded = json_decode($resp['body'], true);
         if ($decoded['code'] != 200) {
             throw new RuntimeException($decoded['message']);
@@ -401,7 +403,7 @@ class Robot
      */
     public function getSpNumbers(): array
     {
-        $resp    = HttpClient::get($this->app->url('/api/v1/spNumbers'), []);
+        $resp = HttpClient::get($this->app->url('/api/v1/spNumbers'), []);
         $decoded = json_decode($resp['body'], true);
 
         if ($decoded['code'] != 200) {
@@ -415,7 +417,7 @@ class Robot
      */
     public function getWorkTimes(): array
     {
-        $resp    = HttpClient::get($this->app->url('/api/v1/worktimes'), []);
+        $resp = HttpClient::get($this->app->url('/api/v1/worktimes'), []);
         $decoded = json_decode($resp['body'], true);
 
         if ($decoded['code'] != 200) {
@@ -429,7 +431,7 @@ class Robot
      */
     public function getSpeechTechniques(): array
     {
-        $resp    = HttpClient::get($this->app->url('/api/v1/speechTechniques'), []);
+        $resp = HttpClient::get($this->app->url('/api/v1/speechTechniques'), []);
         $decoded = json_decode($resp['body'], true);
 
         if ($decoded['code'] != 200) {
@@ -443,7 +445,7 @@ class Robot
      */
     public function getOutboundCustomFieldTemplates(): array
     {
-        $resp    = HttpClient::get($this->app->url('/api/v1/outboundCustomFieldTemplates'), []);
+        $resp = HttpClient::get($this->app->url('/api/v1/outboundCustomFieldTemplates'), []);
         $decoded = json_decode($resp['body'], true);
 
         if ($decoded['code'] != 200) {
@@ -457,7 +459,7 @@ class Robot
      */
     public function getClassification(): array
     {
-        $resp    = HttpClient::get($this->app->url('/api/v1/autoCallTasks/classification'), []);
+        $resp = HttpClient::get($this->app->url('/api/v1/autoCallTasks/classification'), []);
         $decoded = json_decode($resp['body'], true);
 
         if ($decoded['code'] != 200) {
